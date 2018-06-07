00c6aff99553b06cc6e7e10e2f4b4e7e20a5eee0
Amitmcapu <40025614+Amitmcapu@users.noreply.github.com>
2018-06-07 16:32:40 +0530

    Files uploaded

---
+++ truffle.js
@@ -0,0 +1,16 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "5777",
+      from :"0xed318cEbeca36AF65A827d0dAAE5BEe2487d7d98"
+    },
+    ganache: {
+       host:"127.0.0.1",
+       port: 7545,
+       network_id: "5777"
+
+    }
+  }
+};
